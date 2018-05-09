3647433d1a4dc077018b41e906988afdc92262f2
wnsgus313 <wnsgus313@naver.com>
2018-05-09 22:36:49 +0900

    draw a cat

---
+++ BAEKJOON/10171/10171.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+
+    return 0;
+}
+
