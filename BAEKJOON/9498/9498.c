0b43db9fadf23a10df93ed0446b4aebd2a1dffec
wnsgus313 <wnsgus313@naver.com>
2018-05-18 23:35:22 +0900

    print score to grade

---
+++ BAEKJOON/9498/9498.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int score = 0;
+    
+    scanf("%d", &score);
+    
+    if(score >= 90)
+        printf("A\n");
+    else if (score >= 80)
+        printf("B\n");
+    else if (score >= 70)
+        printf("C\n");
+    else if (score >= 60)
+        printf("D\n");
+    else
+        printf("F\n");
+    return 0;
+}
