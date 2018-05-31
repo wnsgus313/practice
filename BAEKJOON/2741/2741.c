95d355c5037549cae6467e8d17317adcf9d2c613
wnsgus313 <wnsgus313@naver.com>
2018-05-31 18:20:42 +0900

    use for

---
+++ BAEKJOON/2741/2741.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    int n = 0;
+    
+    scanf("%d", &n);
+    
+    for(i=1; i<=n; i++){
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
