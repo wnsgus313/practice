c0d056453778c003c08558d1fda78963ed2324cc
wnsgus313 <wnsgus313@naver.com>
2018-06-12 10:50:56 +0900

    use mod

---
+++ BAEKJOON/10430/10430.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) { 
+    int A, B, C;
+    
+    scanf("%d %d %d", &A, &B, &C);
+    
+    printf("%d\n%d\n%d\n%d\n", (A+B)%C, (A%C + B%C)%C, (A*B)%C, (A%C * B%C)%C);
+    
+    return 0;
+}
